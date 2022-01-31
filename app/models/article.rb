38e644010bbad3498c7835af0fc6c00e3e357290
Deepak Mahajan <deepakmahajan269@gmail.com>
2022-01-31 13:27:32 +0530

    updated

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ApplicationRecord
   include Visible
 
-  has_many :comments
+  has_many :comments, dependent: :destroy
   validates_associated :comments
 
   validates :title, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root "articles#index"
 
   resources :articles do
+    member do 
+      get "preview"
+    end
     resources :comments
   end
 
